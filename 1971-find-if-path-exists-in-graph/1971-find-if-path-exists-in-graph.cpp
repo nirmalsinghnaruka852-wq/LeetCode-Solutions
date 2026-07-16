4a83ebca60a8daa4242fc4725afec472a0a8eaaa
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-07-16 21:33:54 +0530

    Time: 408 ms (34.02%), Space: 311.6 MB (41.38%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -20,7 +20,7 @@ public:
  
     
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-      if(source == destination ) return true ;
+      // if(source == destination ) return true ;
         vector<vector<int>>adj(n);
         for(auto edge : edges){
 
