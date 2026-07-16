fe493fbe393157ad79564c7b7add3fbc70fa5e58
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-07-16 21:46:19 +0530

    Time: 1992 ms (5.01%), Space: 235.4 MB (92.53%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
-   bool isHave(int index , vector<vector<int>>& adj , vector<bool>&visited , int destination ){
-    queue<int>q ;
-    q.push(index);
-    visited[index]= true ;
-    while(!q.empty()){
-      auto node = q.front();
-      q.pop() ;
-        if(node  == destination ) return true ;  
-      for(auto edge : adj[node]){
-        if(visited[edge]) continue ;
-         q.push(edge);
-         visited[edge] = true ; 
-      
-      }
-    } 
-    return false ;
+    int find(int index , vector<int>&parent ){
+      if(index == parent[index]) return index ;
+      return find(parent[index] , parent);
     }
- 
-    
+    void union1(int u, int v, vector<int>& parent) {
+    int root1 = find(u, parent);
+    int root2 = find(v, parent);
+
+    if (root1 != root2) {
+        parent[root2] = root1;
+    }
+}
+  
+
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-      // if(source == destination ) return true ;
-        vector<vector<int>>adj(n);
-        for(auto edge : edges){
-
-          adj[edge[0]].push_back(edge[1]) ;
-          adj[edge[1]].push_back(edge[0]);
-        } 
-        vector<bool>visited(n , false );
-       return isHave(source , adj , visited , destination );
-
+      vector<int>parent(n , 0 );
+      for(int index = 0 ; index < n  ; index++){
+        parent[index] = index;
+      }  
+      for(auto edge : edges ){
+          union1(edge[0] , edge[1] , parent);
+      }
+      return find(source, parent) == find(destination , parent);
     }
 };
-//  the first task is convert the edge list into the adj list ;
-//  now we have the adj list 
