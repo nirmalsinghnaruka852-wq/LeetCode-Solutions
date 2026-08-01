d41803b686a2e4cd64259318b286e9fbb62b2f6c
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-08-01 18:39:05 +0530

    Time: 0 ms (100%), Space: 10.2 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,18 @@
 class Solution {
-   public:
-       bool dfs(int left , int right , int player1 , int player2 , char chance,
-         vector<int>&nums){
-          cout << left << " " << right << endl ; 
-        if(left > right) return player1 >= player2;
-        if(chance == '1')
-        return dfs(left + 1 , right , player1 + nums[left] , player2 , '2' , nums)|| 
-             dfs(left , right -1 , player1 + nums[right] , player2 , '2' ,nums);
-        else return  dfs(left + 1 , right , player1 , player2 + nums[left] , '1', nums)&& 
-             dfs(left , right -1 , player1 , nums[right] + player2 , '1' ,nums); 
-       }
+public:
+    vector<vector<int>> dp;
+    int Minmax(int left, int right, vector<int>& nums) {
+        if (left == right)
+            return nums[left];
+        else if (dp[left][right] != -1)
+            return dp[left][right];
+        int LeftPick = nums[left] - Minmax(left + 1, right, nums);
+        int rightPick = nums[right] - Minmax(left, right - 1, nums);
+        return dp[left][right] = max(LeftPick, rightPick);
+    }
     bool predictTheWinner(vector<int>& nums) {
-      // if(nums.size ==1 ) return true ; 
-       return dfs(0 , nums.size()-1 , 0 , 0 , '1' , nums );  
+        int n = nums.size();
+        dp.resize(n, vector<int>(n, -1));
+        return Minmax(0 ,  n-1 , nums ) >= 0 ;
     }
 };
