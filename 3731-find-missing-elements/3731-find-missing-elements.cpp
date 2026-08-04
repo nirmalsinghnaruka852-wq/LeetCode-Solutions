77840c2277259e62152ac90abd3c927875821c32
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-08-04 09:55:43 +0530

    Time: 0 ms (100%), Space: 32.9 MB (40.31%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    vector<int> findMissingElements(vector<int>& nums) {
+    vector<int> findMissingElements(vector<int>& nums){
+      int n = 101 ;
+      vector<int>count(n , 0);
      int Min = INT_MAX;
      int Max = INT_MIN;
      for(int num : nums){
+      count[num] = 1 ; 
       Min = min(Min , num);
       Max = max(Max , num);
      }
-     set<int>st(nums.begin() , nums.end() );
+    //  set<int>st(nums.begin() , nums.end() );
      vector<int>ans ; 
      for(int element = Min + 1 ; element < Max ; element++){
-      if(!st.count(element)) ans.push_back(element);
+      if(count[element] == 0 ) ans.push_back(element);
      }
      return ans ;   
     }
 };
+//  optimaztion ; 
