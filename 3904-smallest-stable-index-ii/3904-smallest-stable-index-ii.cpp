446344edf1beba46ffbb99908bcb930a9c9da82d
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-08-08 10:51:41 +0530

    Time: 39 ms (5.4%), Space: 215.5 MB (8.21%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+   int find(int index , vector<int>& nextSmaller ){
+     if(index == nextSmaller[index]) return index ;
+     return nextSmaller[index] = find(nextSmaller[index] , nextSmaller);
+   }
+    int firstStableIndex(vector<int>& nums, int k) {
+      int n = nums.size();
+     vector<int>nextSmaller(n);
+      for(int index = 0 ; index < n ; index++){
+        nextSmaller[index] = index ; 
+      }
+      stack<int>st ;
+      for(int index = 0 ; index < n ; index++){
+        int currentElement = nums[index];
+        while(st.size()>= 1 && currentElement < nums[st.top()]){
+          nextSmaller[st.top()] = index ;
+          st.pop(); 
+        }
+        st.push(index); 
+      }
+      for(int index = 0 ; index < n ; index++){
+        // cout << index << "->" << nextSmaller[index] << endl;
+      }
+      int Max = INT_MIN ;
+      int MinOne = INT_MAX;
+      for(int index = 0 ; index < n ; index++){
+        Max = max(Max , nums[index]);
+        int i = index ; 
+        
+        
+        // while(i != nextSmaller[i])  i = nextSmaller[i];
+        int SmallestNUmber = nums[find(i, nextSmaller)];
+        int differ = Max - SmallestNUmber ;
+         if(differ <= k ) return index ;
+      }
+       return -1 ; 
+    }
+};
