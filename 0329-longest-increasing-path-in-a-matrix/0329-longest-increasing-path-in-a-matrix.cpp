0eaedbe843996aeeba63b45ad81b81a6075d4c31
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-08-25 07:00:08 +0530

    Time: 4 ms (91.82%), Space: 20.8 MB (74.56%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,53 @@
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vpii = vector<pair<int, int>>;
+using pii = pair<int, int>;
+
+class Solution {
+public:
+    vvi dp;
+
+    vpii dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+
+    bool checkboundry(int row, int col, int n, int m) {
+        return row < 0 || col < 0 || row >= n || col >= m;
+    }
+
+    int dfs(int row, int col, int n, int m, vvi& matrix) {
+        if (dp[row][col] != INT_MIN)
+            return dp[row][col];
+
+        int MaxOne = 1;
+
+        for (pii& dir : dirs) {
+            int r = row + dir.first;
+            int c = col + dir.second;
+
+            if (checkboundry(r, c, n, m) ||
+                matrix[r][c] <= matrix[row][col])
+                continue;
+
+            MaxOne = max(MaxOne, 1 + dfs(r, c, n, m, matrix));
+        }
+
+        return dp[row][col] = MaxOne;
+    }
+
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        dp.resize(n, vector<int>(m, INT_MIN));
+
+        int maxOne = 0;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                maxOne = max(maxOne,
+                             dfs(row, col, n, m, matrix));
+            }
+        }
+
+        return maxOne;
+    }
+};
