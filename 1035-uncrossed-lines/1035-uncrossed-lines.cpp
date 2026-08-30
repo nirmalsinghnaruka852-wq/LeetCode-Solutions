53d806d7de356f41ef8cc475ce4ea56593004d66
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-08-30 19:53:59 +0530

    Time: 11 ms (32.12%), Space: 16.5 MB (72.82%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -5,7 +5,7 @@ public:
     int  n ;
     int m;
     vvi dp ;
-    int maxiumUnCrossedConnection(int i  , int j , vi arr1 , vi arr2 ){
+    int maxiumUnCrossedConnection(int i  , int j , vi& arr1 , vi& arr2 ){
       if(i >= n || j >= m) return 0 ;
       if(dp[i][j] != -1) return dp[i][j];
       if(arr1[i] == arr2[j] ) return dp[i][j] = 1+ maxiumUnCrossedConnection(i+1 , j+1  , arr1 ,arr2);
