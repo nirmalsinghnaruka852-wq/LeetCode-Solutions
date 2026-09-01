37a8ff4ae7f48803189219f07a70ef7e2a95199a
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-09-01 11:41:19 +0530

    Time: 25 ms (6.58%), Space: 17.1 MB (11.96%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,29 +1,37 @@
-class Node{
-    public : 
-    char character ;
-    int Occuerence ;
-    Node(char character , int Occuerence ) : character(character) ,Occuerence(Occuerence){}
+class Node {
+public:
+    char character;
+    int Occuerence;
+    Node(char character, int Occuerence)
+        : character(character), Occuerence(Occuerence) {}
 };
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        stack<Node*>st ;
-        for(int index =0 ; index < s.size() ; index++ ){
+        stack<Node*> st;
+        for (int index = 0; index < s.size(); index++) {
             char ch = s[index];
-            if(!st.empty() && st.top()->character == ch)
-                st.top()->Occuerence++ ;
-            else st.push(new Node(ch  ,  1 ));
-            if(st.top()->Occuerence == k) st.pop() ; 
+            if (!st.empty() && st.top()->character == ch)
+                st.top()->Occuerence++;
+            else
+                st.push(new Node(ch, 1));
+            if (st.top()->Occuerence == k) {
+                Node* node = st.top();
+                st.pop();
+                delete node;
+            }
         }
-         string ans = ""; 
-        while(!st.empty()){
-          while(st.top()->Occuerence != 0 ){
-            ans += st.top()->character ;
-            st.top()->Occuerence-- ; 
-          }
-          st.pop() ;   
+        string ans = "";
+        while (!st.empty()) {
+            Node* node = st.top();
+            st.pop();
+            while (node->Occuerence != 0) {
+                ans += node->character;
+                node->Occuerence--;
+            }
+            delete node;
         }
-        reverse(ans.begin() , ans.end() );
-        return ans ; 
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
