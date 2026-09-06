8de6d8c6e6282b81423712186897b755ac978c3a
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2026-09-06 11:21:01 +0530

    Time: 120 ms (30.53%), Space: 109.6 MB (6.01%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,42 @@
+//  the same thinge but whenever we sell then that then we have to add the profit ; 
+using vi = vector<int>;
+using vvi = vector<vi>;
+class Solution {
+public:
+    vvi dp;
+    int n;
+    vi price ; 
+    int fees ; 
+    int solve(int i = 0  , int flag = 1 ) {
+        if (i >= n)
+            return 0;
+        if (dp[i][flag] != -1)
+            return dp[i][flag];
+        int MaxOne = INT_MIN;
+        if (flag == 1) {
+            // mean we have liberty to buy that stock and do nothing ;
+            int buy = -price[i] + solve(i + 1, 0);
+            int do_nothing = solve(i + 1, flag);
+            MaxOne = max(buy, do_nothing);
+        } else {
+            //  mean have to sell or do nothing ;
+            int sell = price[i] + solve(i + 1, 1) - fees;
+            int do_nothing = solve(i + 1, flag);
+            MaxOne = max(sell, do_nothing);
+        }
+        return dp[i][flag] = MaxOne;
+    }
+    int maxProfit(vector<int>& prices, int fee){
+        n = prices.size();
+        dp.resize(n, vi(2, -1));
+        price = prices ;
+        fees = fee ; 
+        return solve();
+    }
+};
+
+//  we can buy multiple stock , but at a certien point we only have one stock ;
+//  after selling we can't buy stock next day ;
+//  what is your intition ok firstly we have to discuse about it then we will
+//  move futher and also that is feasible or not for the current siwtecthion ;
+
